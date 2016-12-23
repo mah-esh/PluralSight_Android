24ede6b9aa84cf427320bc00c699d3322736cc13
mah-esh <maheswara582s@gmail.com>
2016-12-23 22:01:55 +0530

    added launcher icon

---
+++ HelloWorld/app/src/main/java/com/andro/mahesh/helloworld/MainActivity.java
@@ -0,0 +1,36 @@
+package com.andro.mahesh.helloworld;
+
+
+
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    private TextView greetingView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        greetingView = (TextView) findViewById(R.id.greeting);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_bye){
+//TODO: say bye
+            greetingView.setText(R.string.bye);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
